7975d8089802831eb797d7b39e984e55dc2711ae
Mike Krüger <mikekrueger81@gmail.com>
2016-08-17 08:20:24 +0200

    add the modified archiso configs

---
+++ airootfs/root/customize_airootfs.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+USER="liveuser"
+
+set -e -u
+
+sed -i 's/#\(de_DE\.UTF-8\)/\1/' /etc/locale.gen
+locale-gen
+
+ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+
+usermod -s /usr/bin/zsh root
+cp -aT /etc/skel/ /root/
+chmod 700 /root
+
+groupadd -r autologin
+groupadd -r nopasswdlogin
+
+id -u $USER &>/dev/null || useradd -m $USER -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,autologin,nopasswdlogin,power,wheel"
+passwd -d $USER
+
+echo 'Created User'
+
+
+pushd /home/$USER
+echo "exec startxfce4" >> .xinitrc
+popd
+
+sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
+sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
+sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
+
+sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
+sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
+sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
+
+#Enable Mirrorlist
+sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
+
+#set default Browser
+export _BROWSER=firefox
+echo "BROWSER=/usr/bin/${_BROWSER}" >> /etc/environment
+echo "BROWSER=/usr/bin/${_BROWSER}" >> /etc/skel/.bashrc
+echo "BROWSER=/usr/bin/${_BROWSER}" >> /etc/profile
+
+
+#Set Nano Editor
+export _EDITOR=nano
+echo "EDITOR=${_EDITOR}" >> /etc/environment
+echo "EDITOR=${_EDITOR}" >> /etc/skel/.bashrc
+echo "EDITOR=${_EDITOR}" >> /etc/profile
+
+
+#Enable Sudo
+chmod 750 /etc/sudoers.d
+chmod 440 /etc/sudoers.d/g_wheel
+chown -R root /etc/sudoers.d
+echo "Enabled Sudo"
+
+
+#Setup Pacman
+pacman-key --init archlinux
+pacman-key --populate archlinux
+
+
+#Enable Services
+systemctl enable pacman-init.service lightdm.service choose-mirror.service dhcpcd.service
+systemctl enable org.cups.cupsd.service
+systemctl enable avahi-daemon.service
+systemctl -fq enable NetworkManager
+systemctl mask systemd-rfkill@.service
+systemctl set-default graphical.target
